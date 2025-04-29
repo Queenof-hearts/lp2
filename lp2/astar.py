ca215c516cbc47c1690203d91dbd3f9267b50094
Queenof-hearts <168013824+Queenof-hearts@users.noreply.github.com>
2025-04-30 01:24:10 +0530

    Update astar.py

--- lp2/astar.py
+++ lp2/astar.py
@@ -1,70 +1,187 @@
-class Graph:
-    def __init__(self,adjacency_list):
-        self.adjacency_list = adjacency_list
+# class Graph:
+#     def __init__(self,adjacency_list):
+#         self.adjacency_list = adjacency_list
 
-    def get_neighbour(self,v):
-        return self.adjacency_list[v]
+#     def get_neighbour(self,v):
+#         return self.adjacency_list[v]
     
-    def h(self,n):
-        H={
-            'A':1,
-            'B':1,
-            'C':1,
-            'D':1
-        }
-        return H[n]
+#     def h(self,n):
+#         H={
+#             'A':1,
+#             'B':1,
+#             'C':1,
+#             'D':1
+#         }
+#         return H[n]
     
-    def a_star(self,start_node,stop_node):
-        open_list=set([start_node])
-        close_list=set([])
-        g={start_node:0}
-        parents={start_node:start_node}
+#     def a_star(self,start_node,stop_node):
+#         open_list=set([start_node])
+#         close_list=set([])
+#         g={start_node:0}
+#         parents={start_node:start_node}
         
-        while open_list:
-            n =None
-            for v in open_list:
-                if n is None or g[v]+self.h(v)<g[n]+self.h(n):
-                    n=v
+#         while open_list:
+#             n =None
+#             for v in open_list:
+#                 if n is None or g[v]+self.h(v)<g[n]+self.h(n):
+#                     n=v
 
             
-            if n is None:
-                print('Path does not exist!')
-                return None
+#             if n is None:
+#                 print('Path does not exist!')
+#                 return None
             
-            if n==stop_node:
-                path=[]
-                while parents[n]!=n:
-                    path.append(n)
-                    n=parents[n]
-                path.append(start_node)
-                path.reverse()
-                print("path found {}".format(path))
-                return path
+#             if n==stop_node:
+#                 path=[]
+#                 while parents[n]!=n:
+#                     path.append(n)
+#                     n=parents[n]
+#                 path.append(start_node)
+#                 path.reverse()
+#                 print("path found {}".format(path))
+#                 return path
 
-            for (m,weight) in self.get_neighbour(n):
-                if m not in open_list and m not in close_list:
-                    open_list.add(m)
-                    parents[m]=n
-                    g[m]=g[n]+weight
-                else:
-                    if(g[m]>g[n]+weight):
-                        g[m]=g[n]+weight
-                        parents[m]=n
-                        if m in close_list:
-                            close_list.remove(m)
-                            open_list.add(m)
+#             for (m,weight) in self.get_neighbour(n):
+#                 if m not in open_list and m not in close_list:
+#                     open_list.add(m)
+#                     parents[m]=n
+#                     g[m]=g[n]+weight
+#                 else:
+#                     if(g[m]>g[n]+weight):
+#                         g[m]=g[n]+weight
+#                         parents[m]=n
+#                         if m in close_list:
+#                             close_list.remove(m)
+#                             open_list.add(m)
 
-            open_list.remove(n)
-            close_list.add(n)
+#             open_list.remove(n)
+#             close_list.add(n)
 
-        print('Path does not exist!')
-        return None
+#         print('Path does not exist!')
+#         return None
 
-adjacency_list = {
-    'A': [('B', 1), ('C', 3), ('D', 7)],
-    'B': [('D', 5)],
-    'C': [('D', 12)]
-}
+# adjacency_list = {
+#     'A': [('B', 1), ('C', 3), ('D', 7)],
+#     'B': [('D', 5)],
+#     'C': [('D', 12)]
+# }
 
-graph1=Graph(adjacency_list)
-graph1.a_star('A','D')
+# graph1=Graph(adjacency_list)
+# graph1.a_star('A','D')
+
+g=0
+def print_board(elements):
+    for i in range(9):
+        if i%3 == 0:
+            print()
+        if elements[i]==-1:
+            print("_", end = " ")
+        else:
+            print(elements[i], end = " ")
+    print()
+
+def solvable(start):
+    inv=0
+
+    for i in range(9):
+        if start[i] <= 1:
+            continue
+        for j in range(i+1,9):
+            if start[j]==-1:
+                continue
+            if start[i]>start[j]:
+                inv+=1
+    if inv%2==0:
+        return True
+    return False
+
+def heuristic(start,goal):
+    global g
+    h = 0
+    for i in range(9):
+        for j in range(9):
+            if start[i] == goal[j] and start[i] != -1:
+                h += (abs(j-i))//3 + (abs(j-i))%3
+    return h + g
+
+def moveleft(start,position):
+    start[position],start[position-1]= start[position-1],start[position]
+
+def moveright(start,position):
+    start[position],start[position+1]= start[position+1],start[position]
+
+def moveup(start,position):
+    start[position],start[position-3]= start[position-3],start[position]
+
+def movedown(start,position):
+    start[position],start[position+3]= start[position+3],start[position]
+
+def movetile(start,goal):
+    emptyat= start.index(-1)
+    row = emptyat//3
+    col = emptyat%3
+    t1,t2,t3,t4 = start[:],start[:],start[:],start[:]
+    f1,f2,f3,f4 = 100,100,100,100
+
+    if col -1 >=0:
+        moveleft(t1, emptyat)
+        f1 = heuristic(t1, goal)
+    if col+1<3:
+        moveright(t2, emptyat)
+        f2 = heuristic(t2, goal)
+    if row + 1 <3:
+        movedown(t3, emptyat)
+        f3 = heuristic(t3, goal)
+    if row-1>=0:
+        moveup(t4, emptyat)
+        f4 = heuristic(t4, goal)
+
+    min_heuristic = min(f1, f2,f3,f4)
+
+    if f1==min_heuristic:
+        moveleft(start, emptyat)
+    elif f2==min_heuristic:
+        moveright(start, emptyat)
+    elif f3==min_heuristic:
+        movedown(start, emptyat)
+    elif f4 == min_heuristic:
+        moveup(start, emptyat)
+             
+def solveEight(start,goal):
+    global g
+    g+=1
+    movetile(start,goal)
+    print_board(start)
+    f = heuristic(start,goal)
+    if f == g:
+        print("Solved in {} moves".format(f))
+        return
+
+    solveEight(start,goal)
+
+def main():
+    global g
+    start = list()
+    goal = list()
+    print("Enter the start state:(Enter -1 for empty):")
+    for i in range(9):
+        start.append(int(input()))
+
+    print("Enter the goal state:(Enter -1 for empty):")
+    for i in range(9):
+        goal.append(int(input()))
+
+    print_board(start)
+
+    # To check if solvable
+    if solvable(start):
+        solveEight(start,goal)
+        print("Solved in {} moves".format(g))
+    else:
+        print("Not possible to solve")
+
+if __name__ == '__main__':
+    main()
+
+#    start = [3,7,6,5,1,2,4,-1,8]
+#    goal = [5,3,6,7,-1,2,4,1,8]
